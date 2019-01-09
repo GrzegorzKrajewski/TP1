4116460b9429c98a5e10fc4810af6b36d47bde32
GrzegorzKrajewski <173118@edu.p.lodz.pl>
2019-01-09 17:25:29 +0100

    Obiekty danych i ich zarzadzanie.

---
+++ Tp1/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Tp1
+{
+    class DataContext
+    {
+        public List<Wykaz> osoba;
+        public Dictionary<int, Katalog> ksiazki;
+        public ObservableCollection<Zdarzenie> zdarzenia;
+        public List<OpisStanu> stan;
+
+        public DataContext()
+        {
+            osoba = new List<Wykaz>();
+            ksiazki = new Dictionary<int, Katalog>();
+            zdarzenia = new ObservableCollection<Zdarzenie>();
+            stan = new List<OpisStanu>();
+        }
+    }
+}
---
+++ Tp1/DataRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tp1
+{
+    public class DataRepository
+    {
+        private DataContext dataCon { get; set; }
+    }
+}
